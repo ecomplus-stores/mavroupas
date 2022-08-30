b9d1b43ab7996800c422cf58cbcd5450e0074e1f
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-08-30 09:59:28 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,8 @@
 // Add your custom JavaScript for storefront pages here.
+  storefront.on('widget:@ecomplus/widget-minicart', function () {
+    setTimeout(function () {
+      if(document.querySelectorAll('.product__customization--bordado input').length) {
+        document.querySelector('.product__customization--bordado input').maxLength = 20 
+      }
+    }, 800);
+  });
